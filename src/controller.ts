c531f5a38bb83f42577aeb02e75e10e82b03e1e3
Sofia DiGirolamo <sdigiro@udel.edu>
2019-10-28 15:08:12 -0400

    Added code from Silber's repo

---
+++ src/controller.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+export class Controller {
+    public getTask(req: express.Request, res: express.Response): void {
+
+    }
+
+    public postTask(req: express.Request, res: express.Response): void {
+
+    }
+
+    public deleteTask(req: express.Request, res: express.Response): void {
+
+    }
+
+    public updateTask(req: express.Request, res: express.Response): void {
+
+    }
+}
---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { Controller } from "./controller";
+
+export class ApiRouter {
+    private router: express.Router = express.Router();
+    private controller: Controller = new Controller();
+
+    // Creates the routes for this router and returns a populated router object
+    public getRouter(): express.Router {
+        this.router.get("/task", this.controller.getTask);
+        this.router.post("/task", this.controller.postTask);
+        this.router.delete("/task", this.controller.deleteTask);
+        //this.router.update("/task", this.controller.updateTask);
+
+        return this.router;
+    }
+}
