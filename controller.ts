92c67567a2cbafbe69d41ff5782787caa106964d
Betty Zhang <bettyzhang@wifi-roaming-128-4-109-91.host.udel.edu>
2019-11-01 11:38:10 -0400

    communicate withmongo

---
+++ controller.ts
@@ -0,0 +1,27 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Task = new Schema({
+    name: {
+    	  type: String
+},
+Created_date: {
+    type:Date,
+    default: Date.now
+},
+due_date: {
+	  type:Date
+},
+status: {
+   type: [{
+    type: String,
+    enum: ['pending', 'ongoing', 'complete']
+ }]
+},
+priority: {
+	  type: Integer
+}
+});
+
+module.exports = mongoose.model('Tasks', Task);
