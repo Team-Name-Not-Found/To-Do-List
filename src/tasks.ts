2f475df1a2c963bf85b75ed626674bd32b6bddc4
Kate Bagshaw <kbagshaw@udel.edu>
2019-11-13 14:05:56 -0500

    merged with angular project ui

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose');
+import { prop, Typegoose, ModelType, InstanceType } from 'typegoose';
+import * as mongoose from 'mongoose';
 
-var taskSchema = new mongoose.Schema({
+mongoose.connect('mongodb://localhost:27017/');
+
+let taskSchema = new mongoose.Schema({
     task_desc: String,
     date_time: {type: Date, default: Date.now},
     difficulty: String, 
