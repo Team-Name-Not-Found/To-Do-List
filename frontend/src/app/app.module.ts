0ef021f193b2c465b4ba42e0ac63d497dffeeeb1
Betty Zhang <bettyzhang@Bettys-Air.home>
2019-11-28 14:54:12 -0500

    added frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './tasks/list/list.component';
+import { CreateComponent } from './tasks/create/create.component';
+import { EditComponent } from './tasks/edit/edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
